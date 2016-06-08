882c1cc955e5c5ea889e8310ebece7eb4e2a6a48
Alex <alexxnavarretee@gmail.com>
2016-06-07 18:00:31 -0600

    Trelora Services
    Sends request to the Trelora API and receives a list of houses 'comingsoon' or
    'sold' depending on the parameter given to "#house_listing." Default number of
    houses returned is set to '40' but can always be changed when adding a second
    argument within the "#house_listing" method.
    
    closes#12

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,9 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'capybara/rails'
+require 'vcr'
+require 'webmock'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -25,6 +28,12 @@ require 'rspec/rails'
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.allow_http_connections_when_no_cassette = true
+end
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
