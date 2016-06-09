cb0b75572ff24769721493aa8853257736b924f2
Sal Espinosa <sespinos@gmail.com>
2016-06-09 15:52:16 -0600

    Add ability to request new hash after cycle

--- app/assets/javascripts/device.js
+++ app/assets/javascripts/device.js
@@ -1,32 +1,37 @@
 $(document).ready(function(){
   if($("#device-content")) {
-    $.ajax({
-      method: "GET",
-      url: "/api/v1/signage",
-      dataType: "JSON",
-      success: renderContent
-    });
+    pullSlides();
   }
 });
 
+var pullSlides = function() {
+  $.ajax({
+    method: "GET",
+    url: "/api/v1/signage",
+    dataType: "JSON",
+    success: renderContent
+  });
+};
+
+
 var renderContent = function(listingInfo){
-  var device_code = $("#device-content").attr('class');
+  var deviceCode = $("#device-content").attr('class');
 
   if(listingInfo.error){
-    $("#device-content").append(
-      "<h1>Screen ID: " + device_code + "</h1>"
-    );
+    $("#device-content").html("<h1>Screen ID: " + deviceCode + "</h1>");
+    window.setTimeout(function(){pullSlides();}, 10000);
   } else {
     renderSign(0, listingInfo, renderSign);
   }
 };
 
 var renderSign = function(signNumber, listingInfo, callback) {
-  var sign = listingInfo[signNumber];
-  $("#device-content").html("");
-  $("#device-content").append(
-    "<p>" + sign.title + "</p><p>" + sign.ribbon_color + "</p><p>"+
-    sign.ribbon + "</p><p>" + sign.subtitle + "</p>" + "<img src='" + sign.best_large_image +"'>"
-  );
-  window.setTimeout(function(){callback(signNumber + 1, listingInfo, renderSign);}, 4000);
+  if(signNumber == listingInfo.length -1) {
+    pullSlides();
+  } else {
+    var sign = listingInfo[signNumber];
+    $("#device-content").html("<p>" + sign.title + "</p><p>" + sign.ribbon_color + "</p><p>" + sign.ribbon + "</p><p>" + sign.subtitle + "</p>" + "<img src='" + sign.best_large_image +"'>"
+    );
+    window.setTimeout(function(){callback(signNumber + 1, listingInfo, renderSign);}, 1000);
+  }
 };
