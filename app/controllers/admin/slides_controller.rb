fcc7bccaeba86e358911f8353c772322928b51b7
Ryan Workman <rdavid1099@gmail.com>
2017-01-05 09:26:39 -0700

    Testing form for creating slides

--- app/controllers/admin/slides_controller.rb
+++ app/controllers/admin/slides_controller.rb
@@ -4,4 +4,26 @@ class Admin::SlidesController < ApplicationController
     Role.update_roles
     redirect_to admin_devices_path
   end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @slide = Slide.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- spec/features/admin/admin_creates_slide_spec.rb
+++ spec/features/admin/admin_creates_slide_spec.rb
@@ -1,16 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "When an admin clicks create slide" do
-  it "the admin is taken to the slide creation form" do
-    VCR.use_cassette "admin_creates_slide" do
-      user = User.create(email: "admin@email.com", password: "password")
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+RSpec.describe 'When an admin clicks create slide' do
+  let(:user) {User.create(email: "admin@email.com", password: "password")}
 
-      visit "/admin/devices"
+  context 'without ribbon and default background image' do
+    it 'the admin is taken to the slide creation form' do
+      VCR.use_cassette 'admin_creates_slide' do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      click_on "Create Custom Slide"
+        visit '/admin/devices'
 
-      
+        click_on 'Create Custom Slide'
+
+        fill_in 'slide_title', with: 'Test Title'
+        fill_in 'slide_subtitle', with: 'Test Subtitle'
+        fill_in 'slide_display_rate', with: '5'
+        click_on 'Preview Slide'
+
+        expect(current_url).to eq("/admin/slides/#{Slide.last.id}")
+        expect(page).to have_content('Edit Slide')
+        expect(page).to have_content('Create Slide')
+
+        click_on 'Create Slide'
+
+        expect(current_url).to eq('admin/devices')
+        expect(page).to have_content('Slide Successfully Created')
+        expect(page).to have_content('Custom Slides Currently in Active Rotation: 1')
+      end
     end
   end
 end
